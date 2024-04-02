b64b34d25e933a34ffd01e6e025e1a3245a262a4
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-04-02 00:11:07 -0300

    Update versão 3

--- Aplicação em Softw/o-zero-cancela.py
+++ Aplicação em Softw/o-zero-cancela.py
@@ -3,60 +3,40 @@
 # O programa deve exibir soma total, números considerados e números desconsiderados
 #  O programa vai parar quando num < 0 
 
-print(f"\n\tBem vindo ao zero cancela.")
+print(f"\n\tBem vindo(a) ao zero cancela.")
 
-num = soma = num_anterior1 = num_anterior2 = num_anterior3 = 0
+num = soma = num_anterior1 = num_anterior2 = num_anterior3 = zero_consecutivo = 0
 qtd_num = 1
 
-while num >= 0:
+while True:
     num = float(input("Número: "))
 
+    if num < 0:
+        break
 
-    if num > 0:
+    elif num > 0: 
         soma += num
-        conta_zero = 0
-
-        while qtd_num <= 3:
-            if qtd_num == 1:
-                num_anterior1 = num
-                qtd_num += 1
-                break
-
-            elif qtd_num == 2:
-                num_anterior2 = num
-                qtd_num += 1
-                break
-
-            elif qtd_num == 3:
-                num_anterior3 = num
-                qtd_num = 1
-                break
-
-    # for i in range(0,num,3):
-    #     print(i)
+        num_anterior3 = num_anterior2
+        num_anterior2 = num_anterior1
+        num_anterior1 = num
+        continue
 
     elif num == 0:
         # Laço para zeros consecutivos
-        while conta_zero <= 3:
-
-            if conta_zero == 0:
-                conta_zero += 1
-                soma -= num_anterior3
-                break
-
-            elif conta_zero == 1:
-                soma -= num_anterior2
-                conta_zero += 1
-                break
-
-            elif conta_zero == 2:
-                soma -= num_anterior1
-                conta_zero += 1
-                break
-
-            elif conta_zero >=3:
-                print("Só é permitido até 03 zeros consecutivos!!")
-                break
-
-        
+       if zero_consecutivo >= 3:
+           print("Só é permitido 3 números consecutivos!!!")
+           continue
+       elif zero_consecutivo == 0:
+           soma -= num_anterior1
+           zero_consecutivo += 1
+           continue
+       elif zero_consecutivo == 1:
+           soma -= num_anterior2
+           zero_consecutivo += 1
+           continue
+       elif zero_consecutivo == 2:
+           soma -= num_anterior3
+           zero_consecutivo += 1
+           continue
+    
 print(f"Soma total = {soma}")
