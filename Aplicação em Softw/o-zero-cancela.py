7f55ce63e1f52929d05ca7796a4661ebe477400b
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-04-08 15:25:18 -0300

    Zero cancela versão final

--- Aplicação em Softw/o-zero-cancela.py
+++ Aplicação em Softw/o-zero-cancela.py
@@ -7,42 +7,53 @@ print(f"\n\tBem vindo ao zero cancela.")
 # inicialização de variáveis
 num_atual = soma = num_anterior1 = num_anterior2 = num_anterior3 = nums_considerados = nums_desconsiderados = 0
 
-# LAÇO QUE VAI GARANTIR QUE USUÁRIO RODE ATE UM NÚMERO MENOR QUE ZERO 
+# Laço que continuará enquanto o número digitado pelo usuário for maior ou igual a zero
 while num_atual >= 0:
-    # PEDE AO USUÁRIO
+    
+    # Solicita ao usuário um número e armazena na variável num_atual
     num_atual = float(input("Número: "))
     
 
     if num_atual > 0:
-        
+        # Se sim, ele é considerado na soma e incrementa o valor números considerados
         soma += num_atual
         nums_considerados += 1
 
-        # HISTÓRICO DE NÚMEROS
+        # Atualiza o histórico dos três últimos números digitados
         num_anterior3 = num_anterior2
         num_anterior2 = num_anterior1
         num_anterior1 = num_atual
 
+        # Reinicia o contador de zeros consecutivos
         zero_consecutivo = 0
     
+    # Verifica se o número digitado é igual a zero
     elif num_atual == 0:
+        # Se sim, verifica se já houve três zeros consecutivos
         if zero_consecutivo == 3:
             print("Apenas 3 zeros consecutivos.")
         else:
+            # Se não, incrementa o contador de números desconsiderados e decrementa o considerado
             nums_desconsiderados += 1
             nums_considerados -= 1
 
+            # Subtrai o último número considerado da soma
             soma -= num_anterior1 
+            
+             # Atualiza o histórico dos três últimos números digitados
             num_anterior1 = num_anterior2
             num_anterior2 = num_anterior3
             num_anterior3 = 0
 
+             # Incrementa o contador de zeros consecutivos 
             zero_consecutivo += 1 
-                
+
+# Verifica se não houve números considerados na soma                
 if nums_considerados <=  0:
     nums_considerados = 0
     soma = 0
 
+# Imprime os resultados
 print(f"Soma = {soma}")
 print(f"Numeros considerados: {nums_considerados}")
 print(f"Numeros desconsiderados: {nums_desconsiderados}")
