9d050174d546eae6ebefb20a8fb421d0ddd2f7fa
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-04-08 09:28:28 -0300

    Update Zero cancela versão 2

--- Aplicação em Softw/o-zero-cancela.py
+++ Aplicação em Softw/o-zero-cancela.py
@@ -1,42 +1,61 @@
 #  Construir um programa que calcule a soma de uma sequencia de números digitado pelo usuário, 
 #  Quando o usuário digitar 0 vai cancelar o último digito, o usuário pode cancelar 3 vezes consecutivas
 # O programa deve exibir soma total, números considerados e números desconsiderados
-#  O programa vai parar quando num < 0 
+#  O programa vai parar quando num_atual < 0 
 
-print(f"\n\tBem vindo(a) ao zero cancela.")
+print(f"\n\tBem vindo ao zero cancela.")
+# inicialização de variáveis
+num_atual = soma = num_anterior1 = num_anterior2 = num_anterior3 = nums_considerados = nums_desconsiderados = 0
 
-num = soma = num_anterior1 = num_anterior2 = num_anterior3 = zero_consecutivo = 0
-qtd_num = 1
+# LAÇO QUE VAI GARANTIR QUE USUÁRIO RODE ATE UM NÚMERO MENOR QUE ZERO 
+while num_atual >= 0:
+    # PEDE AO USUÁRIO
+    num_atual = float(input("Número: "))
+    
 
-while True:
-    num = float(input("Número: "))
+    if num_atual > 0:
+        
+        soma += num_atual
+        nums_considerados += 1
 
-    if num < 0:
-        break
-
-    elif num > 0: 
-        soma += num
+        # HISTÓRICO DE NÚMEROS
         num_anterior3 = num_anterior2
         num_anterior2 = num_anterior1
-        num_anterior1 = num
-        continue
+        num_anterior1 = num_atual
 
-    elif num == 0:
-        # Laço para zeros consecutivos
-       if zero_consecutivo >= 3:
-           print("Só é permitido 3 números consecutivos!!!")
-           continue
-       elif zero_consecutivo == 0:
-           soma -= num_anterior1
-           zero_consecutivo += 1
-           continue
-       elif zero_consecutivo == 1:
-           soma -= num_anterior2
-           zero_consecutivo += 1
-           continue
-       elif zero_consecutivo == 2:
-           soma -= num_anterior3
-           zero_consecutivo += 1
-           continue
+        zero_consecutivo = 0
     
-print(f"Soma total = {soma}")
+    elif num_atual == 0:
+        if zero_consecutivo == 3:
+            print("Apenas 3 zeros consecutivos.")
+        else:
+            nums_desconsiderados += 1
+            nums_considerados -= 1
+            
+            # if zero_consecutivo == 0:
+            #     soma -= num_anterior1
+                
+            
+            if zero_consecutivo == 1:
+                num_anterior1 = num_anterior2
+                num_anterior2 = num_anterior3
+                num_anterior3 = 0
+                       
+
+           
+
+            elif zero_consecutivo == 2:
+                num_anterior1 = num_anterior2
+                num_anterior2 = 0
+              
+
+            soma -= num_anterior1  
+            zero_consecutivo += 1 
+                
+if nums_considerados <=  0:
+    nums_considerados = 0
+    soma = 0
+
+print(f"Soma = {soma}")
+print(f"Numeros considerados: {nums_considerados}")
+print(f"Numeros desconsiderados: {nums_desconsiderados}")
