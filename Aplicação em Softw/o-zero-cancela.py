8060f26b9cddf62fa254ccd018bd738b86f3cd26
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-04-15 05:35:38 -0300

    final

--- Aplicação em Softw/o-zero-cancela.py
+++ Aplicação em Softw/o-zero-cancela.py
@@ -1,59 +1,53 @@
-#  Construir um programa que calcule a soma de uma sequencia de números digitado pelo usuário, 
-#  Quando o usuário digitar 0 vai cancelar o último digito, o usuário pode cancelar 3 vezes consecutivas
-# O programa deve exibir soma total, números considerados e números desconsiderados
-#  O programa vai parar quando num_atual < 0 
+print(f"\n\tBem vindo(a) ao zero cancela.")
 
-print(f"\n\tBem vindo ao zero cancela.")
 # inicialização de variáveis
-num_atual = soma = num_anterior1 = num_anterior2 = num_anterior3 = nums_considerados = nums_desconsiderados = 0
+num_atual = soma = ant1 = ant2 = ant3 = nums_considerados = nums_desconsiderados = zeros_consecutivos = 0
 
-# Laço que continuará enquanto o número digitado pelo usuário for maior ou igual a zero
-while num_atual >= 0:
-    
+while num_atual >= 0: 
+
     # Solicita ao usuário um número e armazena na variável num_atual
-    num_atual = float(input("Número: "))
-    
+    num_atual = float(input(f"\nNúmero: "))
 
     if num_atual > 0:
         # Se sim, ele é considerado na soma e incrementa o valor números considerados
         soma += num_atual
+        print(f"Soma dos números adicionados: {soma}")
         nums_considerados += 1
 
         # Atualiza o histórico dos três últimos números digitados
-        num_anterior3 = num_anterior2
-        num_anterior2 = num_anterior1
-        num_anterior1 = num_atual
+        ant3 = ant2
+        ant2 = ant1
+        ant1 = num_atual
 
         # Reinicia o contador de zeros consecutivos
-        zero_consecutivo = 0
-    
+        zeros_consecutivos = 0
+
     # Verifica se o número digitado é igual a zero
     elif num_atual == 0:
+
+        # Incrementa o contador de zeros consecutivos 
+        zeros_consecutivos += 1 
+
         # Se sim, verifica se já houve três zeros consecutivos
-        if zero_consecutivo == 3:
-            print("Apenas 3 zeros consecutivos.")
-        else:
-            # Se não, incrementa o contador de números desconsiderados e decrementa o considerado
+        if zeros_consecutivos > 3:
+            print(f"Só é possível digitar 3 zeros consecutivos!")
+        elif ant1 > 0:
+            
+            # Se não, subtrai o último número considerado da soma
+            soma -= ant1
+
+            # incrementa o contador de números desconsiderados e decrementa o considerado 
             nums_desconsiderados += 1
             nums_considerados -= 1
 
-            # Subtrai o último número considerado da soma
-            soma -= num_anterior1 
+            # Atualiza o histórico dos três últimos números digitados
+            ant1 = ant2
+            ant2 = ant3
+            ant3 = 0 
+
+            print(f"Nova soma: {soma} | Número desconsiderado: {ant1}")
+        else:
+          print(f"Soma dos números adicionados: {soma}")
+          print(f"Não é possisvel desconsiderar nenhum número da soma!\n")
             
-             # Atualiza o histórico dos três últimos números digitados
-            num_anterior1 = num_anterior2
-            num_anterior2 = num_anterior3
-            num_anterior3 = 0
-
-             # Incrementa o contador de zeros consecutivos 
-            zero_consecutivo += 1 
-
-# Verifica se não houve números considerados na soma                
-if nums_considerados <=  0:
-    nums_considerados = 0
-    soma = 0
-
-# Imprime os resultados
-print(f"Soma = {soma}")
-print(f"Numeros considerados: {nums_considerados}")
-print(f"Numeros desconsiderados: {nums_desconsiderados}")
+print(f"\n\tZero cancela interrompido por {num_atual}\n |Soma = {soma}\n |Números considerados: {nums_considerados}\n |Números desconsiderados: {nums_desconsiderados}")
